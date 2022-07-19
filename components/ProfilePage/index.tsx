a50d7538710939e07b7ec815122aa1fc04cf826a
anderson <anderson.ap133@gmail.com>
2022-07-18 21:57:00 -0300

    Finished ProfilePage Styles

--- components/ProfilePage/index.tsx
+++ components/ProfilePage/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 import {
-  
+        Container,
+        Banner,
+        Avatar,
+        ProfileData,
+        LocationIcon,
+        CakeIcon,
+        Followage,
+        EditButton,
 } from './styles';
 
 const ProfilePage: React.FC = () => {
